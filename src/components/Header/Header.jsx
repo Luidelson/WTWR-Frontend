ecb457e83f1ab243435fa3556818315b8308dd71
Lutrunks <lutrunks11@gmail.com>
2025-06-02 20:57:19 -0700

    react

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import "./Header.css";
+import logo from "../../assets/wtwr.logo.svg";
+import avatar from "../../assets/avatar.logo.png";
+
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      {/* TODOOOOOO fix location */}
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
